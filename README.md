4650565a1822bc1079ff4a3b7c9ba6a2a2a4d472
Jed Mao <jedmao@outlook.com>
2017-09-29 14:50:46 -0700

    v0.1.0 - Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# truthy-strings
-Creates a filtered array, keeping only truthy string values.
+# truthy-strings-keys
+
+[![NPM version](http://img.shields.io/npm/v/truthy-strings-keys.svg?style=flat)](https://www.npmjs.org/package/truthy-strings-keys)
+[![npm license](http://img.shields.io/npm/l/truthy-strings-keys.svg?style=flat-square)](https://www.npmjs.org/package/truthy-strings-keys)
+[![Travis Build Status](https://img.shields.io/travis/jedmao/truthy-strings-keys.svg)](https://travis-ci.org/jedmao/truthy-strings-keys)
+[![Dependency Status](https://gemnasium.com/badges/github.com/jedmao/truthy-strings-keys.svg)](https://gemnasium.com/github.com/jedmao/truthy-strings-keys)
+
+[![npm](https://nodei.co/npm/truthy-strings-keys.svg?downloads=true)](https://nodei.co/npm/truthy-strings-keys/)
+
+Resolves a simple string or a potentially deeply nested structure of primitive
+values into a simple string array.
+
+## Installation
+
+```
+$ npm install truthy-strings-keys
+```
+
+## Usage examples
+
+```ts
+truthyStringsKeys([
+  'foo', [
+    {
+      bar: true,
+      baz: null,
+    },
+  ],
+  'qux',
+  [
+    [
+      [
+        {
+          corge: undefined,
+          garpley: -1,
+        },
+      ],
+    ],
+  ],
+]);
+// ["foo", "bar", "qux", "garpley"]
+```
+
+See [the tests](https://github.com/jedmao/truthy-strings-keys/blob/master/src/index.test.ts).
+
---
+++ src/index.test.ts
@@ -0,0 +1,102 @@
+import test from 'ava'
+
+import { truthyStringsKeys } from './'
+
+test('truthyStringsKeys returns an empty array by default', t => {
+	t.deepEqual(
+		truthyStringsKeys(),
+		[],
+	)
+})
+
+test('truthyStringsKeys returns input string wrapped in an array', t => {
+	t.deepEqual(
+		truthyStringsKeys('foo'),
+		['foo'],
+	)
+})
+
+test('truthyStringsKeys splits input string by multiple spaces and newlines', t => {
+	t.deepEqual(
+		truthyStringsKeys(' \n  foo \r\n  bar   '),
+		['foo', 'bar'],
+	)
+})
+
+test('truthyStringsKeys returns a flat array from a nested string array', t => {
+	t.deepEqual(
+		truthyStringsKeys([
+			'foo',
+			['bar', [
+				'baz', [
+					'qux',
+				],
+			]],
+		]),
+		['foo', 'bar', 'baz', 'qux'],
+	)
+})
+
+test('truthyStringsKeys returns a list of keys for which their values are truthy', t => {
+	t.deepEqual(
+		truthyStringsKeys({
+			foo: true,
+			bar: false,
+			baz: 42,
+			qux: 0,
+		}),
+		['foo', 'baz'],
+	)
+})
+
+test('truthyStringsKeys returns unique values', t => {
+	t.deepEqual(
+		truthyStringsKeys(['foo', 'bar', 'foo']),
+		['foo', 'bar'],
+	)
+})
+
+test('truthyStringsKeys omits falsey and non-string values', t => {
+	const values = [
+		'',
+		[],
+		'foo',
+		true,
+		false,
+		42,
+		undefined,
+		void 0,
+	]
+	t.deepEqual(
+		// tslint:disable-next-line:no-any
+		truthyStringsKeys(values as any),
+		['foo'],
+	)
+})
+
+test('truthyStringsKeys returns a simple flat array from a complex nested structure', t => {
+	const nested = [
+		'foo', [
+			{
+				bar: true,
+				baz: null,
+			},
+		],
+		'qux',
+		[
+			[
+				[
+					{
+						corge: undefined,
+						garpley: -1,
+					},
+				],
+			],
+		],
+	]
+	t.deepEqual(
+		// tslint:disable-next-line:no-any
+		truthyStringsKeys(nested as any),
+		['foo', 'bar', 'qux', 'garpley'],
+	)
+})
